8bbe7b5d1d2deca2da2750ef43131ee5c47bd90d
DongwookKim0823 <dwkim0823@naver.com>
2021-08-05 23:58:34 +0900

    3-15

--- django/mysite/pybo/views/base_views.py
+++ django/mysite/pybo/views/base_views.py
@@ -1,21 +1,28 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
 
 from ..models import Question
 
-def index(request):
 
+def index(request):
     """
     pybo 목록 출력
-
     """
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
 
-    # 조회
-    question_list = Question.objects.order_by('-create_date')
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+    # 검색
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목검색
@@ -28,15 +35,13 @@ def index(request):
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}  # page와 kw가 추가되었다.
-
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}  # <------ so 추가
     return render(request, 'pybo/question_list.html', context)
 
-def detail(request, question_id):
 
+def detail(request, question_id):
     """
     pybo 내용 출력
-
     """
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
